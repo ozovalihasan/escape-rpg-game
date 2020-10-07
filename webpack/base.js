36d395bd091c38cee18d3fa03580985bd68b1f76
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-07 17:29:42 +0300

    Add splitChunks optimization

--- webpack/base.js
+++ webpack/base.js
@@ -37,4 +37,23 @@ module.exports = {
       template: './index.html',
     }),
   ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          // cacheGroupKey here is `commons` as the key of the cacheGroup
+          name(module, chunks, cacheGroupKey) {
+            const moduleFileName = module
+              .identifier()
+              .split('/')
+              .reduceRight((item) => item);
+            const allChunksNames = chunks.map((item) => item.name).join('~');
+            return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+          },
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };

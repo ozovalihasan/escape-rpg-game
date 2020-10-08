276ce3c8f58d279a6507293353166ff7b22d0b94
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-08 12:25:39 +0300

    Update config file

--- src/Config/config.js
+++ src/Config/config.js
@@ -2,7 +2,15 @@ import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
+  parent: 'content',
+  width: 320,
+  height: 240,
+  zoom: 2,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
 };

3138ad86bfc5d5983785aec8f6945de973bd6568
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-13 10:46:38 +0300

    Delete unnecessary comments

--- src/Objects/PlayerCharacter.js
+++ src/Objects/PlayerCharacter.js
@@ -1,27 +1,3 @@
-// import Unit from './Unit';
-
-// var PlayerCharacter = new Phaser.Class({
-//   Extends: Unit,
-
-//   initialize: function PlayerCharacter(
-//     scene,
-//     x,
-//     y,
-//     texture,
-//     frame,
-//     type,
-//     hp,
-//     damage
-//   ) {
-//     Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
-//     // flip the image so I don"t have to edit it manually
-//     this.flipX = true;
-
-//     this.setScale(2);
-//   },
-// });
-// export default PlayerCharacter;
-
 import Unit from './Unit';
 
 export default class PlayerCharacter extends Unit {

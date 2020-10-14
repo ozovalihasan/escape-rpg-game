cea410efc31523010db4a3fb66169d63b95d1619
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-14 11:17:59 +0300

    Update positions of the elements of the scoreboard

--- src/Scenes/ScoreScene.js
+++ src/Scenes/ScoreScene.js
@@ -3,7 +3,6 @@ import Phaser from 'phaser';
 import OperationsAPI from '../Message/OperationsAPI';
 import Button from '../Objects/Button';
 
-
 export default class ScoreScene extends Phaser.Scene {
   constructor() {
     super('Score');
@@ -11,44 +10,49 @@ export default class ScoreScene extends Phaser.Scene {
   create() {
     OperationsAPI.getScores()
       .then((games) => {
-        this.addNames(games.result)
+        this.addNames(games.result);
       })
       .catch((error) => {
         alert(error);
       });
   }
 
-  addNames(games){
-  	const orderedgames = games
-          .sort((game1, game2) => game2.score - game1.score)
-          .slice(0, 5);
+  addNames(games) {
+    this.add
+      .text(config.width / 2, 40, 'High Scores', {
+        fontsize: '32px',
+        fill: '#fff',
+      })
+      .setOrigin(0.5, 0.5);
 
-    this.add.text(config.width/2, 20, 'High Scores', {
-      fontsize: '32px',
-      fill: '#fff',
-    }).setOrigin(0.5, 0.5);
+    const orderedgames = games
+      .sort((game1, game2) => game2.score - game1.score)
+      .slice(0, 5);
 
-    orderedgames.forEach( (game, index)=> {
-      	this.add.text(config.width/2-35, index*30+70, game.user, {
-        	fontsize: '32px',
-        	fill: '#fff',
-      	}).setOrigin(0.5, 0.5);	
+    orderedgames.forEach((game, index) => {
+      this.add
+        .text(config.width / 2 - 35, index * 30 + 100, game.user, {
+          fontsize: '32px',
+          fill: '#fff',
+        })
+        .setOrigin(0.5, 0.5);
 
-      	this.add.text(config.width/2+45, index*30+70, game.score, {
-        	fontsize: '32px',
-        	fill: '#fff',
-      	}).setOrigin(0.5, 0.5);	
-    });	
+      this.add
+        .text(config.width / 2 + 45, index * 30 + 100, game.score, {
+          fontsize: '32px',
+          fill: '#fff',
+        })
+        .setOrigin(0.5, 0.5);
+    });
 
     this.menuButton = new Button(
       this,
-      400,
-      500,
+      config.width / 2,
+      config.height - 50,
       'blueButton1',
       'blueButton2',
       'Menu',
-      'Title',
+      'Title'
     );
-
   }
 }

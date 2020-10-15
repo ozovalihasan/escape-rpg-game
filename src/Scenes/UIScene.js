6270a8eafeee7e259725173542e7033421c6d2cc
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-15 10:50:43 +0300

    Update healthbar positions by using units

--- src/Scenes/UIScene.js
+++ src/Scenes/UIScene.js
@@ -9,82 +9,81 @@ export default class UIScene extends Phaser.Scene {
   constructor() {
     super('UI');
   }
+
   create() {
-    // draw some background for the menu
     this.graphics = this.add.graphics();
     this.graphics.lineStyle(1, 0xffffff);
     this.graphics.fillStyle(0x031f4c, 1);
     this.graphics.strokeRect(
       2,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 4,
-      config.height / 4
+      config.height / 4,
     );
     this.graphics.fillRect(
       2,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 4,
-      config.height / 4
+      config.height / 4,
     );
     this.graphics.strokeRect(
       config.width / 4,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 4,
-      config.height / 4
+      config.height / 4,
     );
     this.graphics.fillRect(
       config.width / 4,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 4,
-      config.height / 4
+      config.height / 4,
     );
     this.graphics.strokeRect(
       config.width / 2,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 2,
-      config.height / 4
+      config.height / 4,
     );
     this.graphics.fillRect(
       config.width / 2,
-      (config.height * 2) / 4,
+      config.height * (2 / 4),
       config.width / 2,
-      config.height / 4
+      config.height / 4,
     );
 
-    // basic container to hold all menus
+
     this.menus = this.add.container();
 
-    this.heroesMenu = new HeroesMenu(195, 153, this);
-    this.actionsMenu = new ActionsMenu(100, 153, this);
-    this.enemiesMenu = new EnemiesMenu(8, 153, this);
+    this.heroesMenu = new HeroesMenu(config.width * (6 / 8), config.height * (2 / 4) + 30, this);
+    this.actionsMenu = new ActionsMenu(config.width * (3 / 8), config.height * (2 / 4) + 30, this);
+    this.enemiesMenu = new EnemiesMenu(config.width / 8, config.height * (2 / 4) + 30, this);
 
-    // the currently selected menu
+
     this.currentMenu = this.actionsMenu;
 
-    // add menus to the container
+
     this.menus.add(this.heroesMenu);
     this.menus.add(this.actionsMenu);
     this.menus.add(this.enemiesMenu);
 
     this.battle = this.scene.get('Battle');
 
-    // listen for keyboard events
+
     this.input.keyboard.on('keydown', this.onKeyInput, this);
 
-    // when its player cunit turn to move
+
     this.battle.events.on('PlayerSelect', this.onPlayerSelect, this);
 
-    // when the action on the menu is selected
-    // for now we have only one action so we dont send and action id
+
     this.events.on('SelectedAction', this.onSelectedAction, this);
 
-    // an enemy is selected
+
     this.events.on('Enemy', this.onEnemy, this);
 
-    // when the scene receives wake event
+
     this.sys.events.on('wake', this.afterWake, this);
 
-    // the message describing the current action
+
     this.message = new Message(this, this.battle.events);
     this.add.existing(this.message);
 
@@ -102,48 +101,42 @@ export default class UIScene extends Phaser.Scene {
       warrior: { unit: this.battle.heroes[0] },
       bluedragon: { unit: this.battle.enemies[0] },
     };
-    this.healthBar.warrior.bar = this.makeBar(190, 0, 0x2ecc71);
-    this.healthBar.warrior.text = this.add.text(
-      290,
-      7,
-      this.battle.heroes[0].hp,
-      { fill: '#0f0' }
-    );
-    this.healthBar.bluedragon.bar = this.makeBar(30, 0, 0x2ecc71);
-    this.healthBar.bluedragon.text = this.add.text(
-      0,
-      7,
-      this.battle.enemies[0].hp,
-      { fill: '#0f0' }
-    );
-    this.updateBars();
-    console.log(this.healthBar);
+    this.healthBar.warrior.bar = this.makeBar(this.healthBar.warrior, 0x2ecc71);
+
+    this.healthBar.bluedragon.bar = this.makeBar(this.healthBar.bluedragon, 0x2ecc71);
 
     this.battle.events.on('UpdateHealthBars', this.updateBars, this);
     this.battle.events.on('DeleteHealthBars', this.deleteBars, this);
   }
 
-  makeBar(x, y, color) {
-    //draw the bar
+  makeBar(unit, color) {
     const bar = this.add.graphics();
     const barBack = this.add.graphics();
-    //color the bar
+
+    bar.x = unit.unit.x - 50;
+    bar.y = unit.unit.y - 60;
+    barBack.x = bar.x;
+    barBack.y = bar.y;
+
+
     bar.fillStyle(color, 1);
     barBack.fillStyle(color, 0.5);
 
-    //fill the bar with a rectangle
+
     bar.fillRect(0, 10, 100, 15);
     barBack.fillRect(0, 10, 100, 15);
 
-    //position the bar
-    bar.x = x;
-    bar.y = y;
-    barBack.x = x;
-    barBack.y = y;
+    unit.text = this.add.text(
+      bar.x + 50,
+      bar.y + 18,
+      unit.unit.hp,
+      { fill: '#0f0' },
+    ).setOrigin(0.5, 0.5);
 
-    //return the bar
+
     return [bar, barBack];
   }
+
   setValue(unit) {
     //scale the bar
     unit.bar[0].scaleX = unit.unit.hp / unit.unit.maxHp;
